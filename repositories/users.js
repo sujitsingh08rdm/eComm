35137dd7f57ea9be5fb5de6a5c7bdc07149b3410
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2024-03-27 08:43:50 +0530

    commit2-1

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
   constructor(filename) {
@@ -23,21 +24,39 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
+
     const records = await this.getAll();
     records.push(attrs);
 
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => {
+      record.id === id;
+    });
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
-  await repo.create({ email: "test@test.com", password: "password" });
+  const user = await repo.getOne("fb84fe58");
 
-  const users = await repo.getAll();
-
-  console.log(users);
+  console.log(user);
 };
 
 test();
